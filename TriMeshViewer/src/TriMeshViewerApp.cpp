51bd9ae8d501173d6711a582724f0377e6e23682
Hai Nguyen <haing@google.com>
2016-08-10 13:36:29 -0700

    Fixed texture and color output

--- TriMeshViewer/src/TriMeshViewerApp.cpp
+++ TriMeshViewer/src/TriMeshViewerApp.cpp
@@ -67,11 +67,11 @@ private:
 
 void TriMeshViewerApp::setup()
 {
-	//mCam.lookAt( vec3( 2, 17, 20 ), vec3( 0, 0, 0 ) );
-	mCam.lookAt( vec3( -15, 4, -15 ), vec3( 0, 0, 0 ) );
-	mNodes = ss::load( getAssetPath( "Untitled_3/Untitled_3.xml" ) );
+	mCam.lookAt( vec3( 8, 14, 14 ), vec3( 0, 0, 0 ) );
+	//mCam.lookAt( vec3( -2, 17, -20 ), vec3( 0, 0, 0 ) );
+	mNodes = ss::load( getAssetPath( "Untitled_1/Untitled_1.xml" ) );
 
-	mTex = gl::Texture2d::create( loadImage( getAssetPath( "textures/photo_1.jpg" ) ) );
+	//mTex = gl::Texture2d::create( loadImage( getAssetPath( "textures/photo_1.jpg" ) ) );
 }
 
 void TriMeshViewerApp::mouseDown( MouseEvent event )
@@ -92,11 +92,11 @@ void TriMeshViewerApp::draw()
 	gl::multViewMatrix( glm::scale( vec3( -1, 1, 1 ) ) );
 
 	//gl::rotate( 0.5f * getElapsedSeconds(), 0, 1, 0 );
-	//gl::rotate( 0.0f * getElapsedSeconds(), 0, 1, 0 );
+	//gl::rotate( 0.2f * getElapsedSeconds(), 1, 0, 0 );
 	//gl::scale( vec3( 1.0f + 0.10f * sin( 2.0f * getElapsedSeconds() ) ) );
 
 	for( auto& node : mNodes ) {
-		gl::ScopedTextureBind texBind( mTex, 0 );
+		//gl::ScopedTextureBind texBind( mTex, 0 );
 		node.getBatch()->getGlslProg()->uniform( "uTex0", 0 );
 		node.draw();
 	}
